52d42e945bb00d425cc4523d4bba29d64c4bedd5
zerow41 <59548168+zerow41@users.noreply.github.com>
2022-01-25 14:19:22 +0900

    Build: merge로 수정된 파일 재수정

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import AuthTemplate from 'components/Auth/AuthTemplate';
+import SignupForm from 'containers/Auth/SignupForm';
 
-const SignUpPage = () => {
-  return <div>회원가입</div>;
+const SignupPage = () => {
+  return (
+    <AuthTemplate>
+      <SignupForm />
+    </AuthTemplate>
+  );
 };
 
-export default SignUpPage;
+export default SignupPage;
