537c82341b1b4112cc5db3f26ff6046d079d9103
ShinChanU <tlscksdn963@naver.com>
2022-05-16 14:16:45 +0900

    Fix: Button, header 분리

--- src/components/Base/Header/AuthHeader.js
+++ src/components/Base/Header/AuthHeader.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Button2 from 'components/common/Button2';
+import AuthPageButton from 'components/common/AuthPageButton';
 import ModalModule from 'components/common/modal/ModalModule';
 import InsertCanvas from './InsertCanvas';
 
@@ -25,12 +25,12 @@ const Header = ({ user, onLogout }) => {
   return (
     <>
       <Div>
-        <Button2 onClick={openModal}>Go Plan</Button2>
-        <Button2 to="/about">About</Button2>
-        <Button2 to="/contact">Contact</Button2>
-        <Button2 to={process.env.PUBLIC_URL + '/notice/noticeList'}>
+        <AuthPageButton onClick={openModal}>Go Plan</AuthPageButton>
+        <AuthPageButton to="/about">About</AuthPageButton>
+        <AuthPageButton to="/contact">Contact</AuthPageButton>
+        <AuthPageButton to={process.env.PUBLIC_URL + '/notice/noticeList'}>
           Notice
-        </Button2>
+        </AuthPageButton>
       </Div>
       <ModalModule
         modalIsOpen={modalIsOpen}
