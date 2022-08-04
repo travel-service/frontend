c8158aeb6ddedae715a586f8c39e2aa14b2337e5
zerow41 <dudtj0410@naver.com>
2022-08-04 15:36:26 +0900

    Feat: 상태 관리 기능 추가 및 수정

--- src/lib/zustand/dirStore.js
+++ src/lib/zustand/dirStore.js
@@ -1,153 +1,184 @@
-import axios from 'axios';
 import create from 'zustand';
 import * as dirAPI from 'lib/api/dir';
-import { persist } from 'zustand/middleware';
 
 // 여행보관함용 store
-export const dirStore = create(
-  //persist(
-  (set, get) => ({
-    currentDirId: 'm', // 현재 클릭한 디렉터리(보여줄 디렉터리)
-    mainPlans: [], // 메인 디렉터리 플랜들
-    userDirs: [], // 유저 디렉터리 이름들(목록)
-    userPlans: [], // 해당 유저 디렉터리 내 플랜
-    trashPlans: [], // 휴지통 내 플랜들
-    checkedPlans: [], // 선택된 플랜 id
-    createUserDir: '', // 생성할 디렉터리 이름
-    changeDirName: '', // 변경할 디렉터리 이름
-    deleteUserDir: {
-      dirId: [], //삭제할 디렉터리 id, 다중체크
-    },
-    movePlans: {
-      dirId: '', //이동할 디렉터리 id
-      planId: [], //플랜 아이디들, 다중 체크
-    },
-    setMainPlans: (input) => {
-      set({ mainPlans: input });
-    },
-    setTrashPlans: (input) => {
-      set({ trashPlans: input });
-    },
-    setUserPlans: (input) => {
-      set({ userPlans: input });
-    },
-    setUserDirs: (input) => {
-      set((state) => ({
-        userDirs: { ...state.userDirs, input },
-      }));
-      // set({ userDirs.mainUserDirectory: input });
-    },
-    setCurrentDir: (input) => {
-      //현재 디렉터리 설정
-      set({
-        currentDirId: input,
-      });
-    },
-    setCurrentCheckedDir: (input) => {
-      // 현재 선택된 디렉터리
-      set({
-        currentCheckedDirs: input,
-      });
-    },
-    setDirName: (input) => {
-      // 디렉터리 이름 변경
-      set({
-        changeDirName: input,
-      });
-    },
-    // planId setting
-    setCheckedPlans: (input) => {
-      // 플랜 선택
-      set({
-        checkedPlans: input,
-      });
-    },
-    setCreateUserDir: (input) => {
-      //사용자 디렉터리 생성
-      set({
-        createUserDir: input,
-      });
-      /*set((state) => ({
-        createUserDir: { ...state.createUserDir, input },
-      }));*/
-    },
-    setDeleteUserDir: (input) => {
-      //사용자 디렉터리 삭제
-      set((state) => ({
-        deleteUserDir: { ...state.deleteUserDir, dirId: input },
-      }));
-    },
-    setMovePlan: (input1, input2) => {
-      //플랜 이동(담기)
-      set((state) => ({
-        movePlans: { ...state.movePlans, dirId: input1, planId: input2 },
-      }));
-    },
+export const dirStore = create((set, get) => ({
+  currentDirId: 'm', // 현재 클릭한 디렉터리(보여줄 디렉터리)
+  mainPlans: {}, // 메인 디렉터리 플랜들
+  userDirs: {}, // 유저 디렉터리 이름들(목록)
+  //userPlans: [], // 해당 유저 디렉터리 내 플랜
+  trashPlans: {}, // 휴지통 내 플랜들
+  checkedPlans: [], // 선택된 플랜 id
+  createUserDir: '', // 생성할 디렉터리 이름
+  changeDirName: '', // 변경할 디렉터리 이름
+  setUserPlans: (input) => {
+    set({ userPlans: input });
+  },
+  setUserDirs: (input) => {
+    set((state) => ({
+      userDirs: { ...state.userDirs, input },
+    }));
+  },
+  setCurrentDir: (input) => {
+    //현재 디렉터리 설정
+    set({ currentDirId: input });
+  },
+  setCheckedPlans: (input) => {
+    // 선택된 플랜
+    set({ checkedPlans: input });
+  },
+  setDirName: (input) => {
+    // 디렉터리 이름 변경
+    set({ changeDirName: input });
+  },
+  setCreateUserDir: (input) => {
+    //사용자 디렉터리 생성
+    set({ createUserDir: input });
+  },
 
-    // maindir 내 플랜들
-    getMainPlans: async () => {
-      const res = await dirAPI.getAllPlans();
-      set({ mainPlans: res });
-    },
-    // 디렉토리 목록
-    getUserDirs: async () => {
-      const res = await dirAPI.getUserDirs();
-      set({ userDirs: res });
-    },
-    // trashdir 내 플랜들
-    getTrashPlans: async () => {
-      const res = await dirAPI.getTrashPlans();
-      set({ trashPlans: res });
-    },
-    // 플랜 삭제
-    postTrash: async () => {
-      const checkedPlans = get().checkedPlans;
-      await dirAPI.postTrash(checkedPlans);
+  // main dir 플랜 불러오기
+  getMainPlans: async () => {
+    const res = await dirAPI.getAllPlans();
+    set({ mainPlans: res });
+  },
+  // user dir 목록 불러오기
+  getUserDirs: async () => {
+    const res = await dirAPI.getUserDirs();
+    set({ userDirs: res });
+  },
+  // trash dir 플랜 불러오기
+  getTrashPlans: async () => {
+    const res = await dirAPI.getTrashPlans();
+    set({ trashPlans: res });
+  },
+  // 플랜 삭제
+  postTrash: async () => {
+    const checkedPlans = get().checkedPlans;
+    const res = await dirAPI.postTrash(checkedPlans);
+
+    if (res.httpStatus === 201) {
       set({ checkedPlans: [] });
-    },
-    // 플랜 복원
-    postRevert: async () => {
-      const checkedPlans = get().checkedPlans;
-      await dirAPI.postRevert(checkedPlans);
+
+      //n개 이상의 플랜 복원 시 데이터 새로 불러오는 것이 낫다고 생각,,
+      const t = await dirAPI.getTrashPlans();
+      if (t.httpStatus === 200) {
+        set({ currentDirId: 't' });
+        set({ trashPlans: t });
+      }
+    }
+  },
+  // 플랜 담기
+  postMovePlans: async (dirId) => {
+    const checkedPlans = get().checkedPlans;
+    const res = await dirAPI.movePlan(checkedPlans, dirId);
+
+    if (res.httpStatus === 201) {
       set({ checkedPlans: [] });
-    },
-    // 플랜 영구 삭제, 수정 필요
-    deletePlan: async () => {
-      const checkedPlans = get().checkedPlans;
-      await dirAPI.deletePlan(checkedPlans);
+      const m = await dirAPI.getAllPlans();
+      if (m.httpStatus === 200) {
+        set({ currentDirId: dirId });
+        set({ mainPlans: m });
+      }
+    }
+  },
+  // 플랜 복원
+  postRevert: async () => {
+    const checkedPlans = get().checkedPlans;
+    const res = await dirAPI.postRevert(checkedPlans);
+
+    if (res.httpStatus === 201) {
       set({ checkedPlans: [] });
-    },
-    // userdir 생성, 수정 필요
-    postCreateDir: async () => {
-      const createUserDir = get().createUserDir;
-      await dirAPI.createDir(createUserDir);
-    },
-    // userdir 삭제, 수정 필요
-    postDeleteDir: async () => {
-      const currentDirId = get().currentDirId;
-      const idArr = new Array();
-      idArr[0] = currentDirId;
-      const res = await dirAPI.deleteDir(idArr);
+
+      const t = await dirAPI.getTrashPlans();
+      const m = await dirAPI.getAllPlans();
+      if (t.httpStatus === 200 && m.httpStatus === 200) {
+        set({ currentDirId: 'm' });
+        set({ trashPlans: t });
+        set({ mainPlans: m });
+      }
+    }
+  },
+  // 플랜 영구 삭제
+  deletePlan: async () => {
+    const checkedPlans = get().checkedPlans;
+    const res = await dirAPI.deletePlan(checkedPlans);
+
+    if (res.httpStatus === 204) {
+      set({ checkedPlans: [] });
+
+      const t = await dirAPI.getTrashPlans();
+      if (t.httpStatus === 201) {
+        set({ currentDirId: 't' });
+        set({ trashPlans: t });
+      }
+    }
+  },
+  // userdir 생성
+  postCreateDir: async () => {
+    const createUserDir = get().createUserDir;
+    const res = await dirAPI.createDir(createUserDir);
+
+    if (res.httpStatus === 201) {
+      set((state) => ({
+        userDirs: {
+          ...state.userDirs,
+          planCount: [...state.userDirs.planCount, 0],
+          mainUserDirectory: [
+            ...state.userDirs.mainUserDirectory,
+            {
+              userDirectoryId: res.userDirectoryId,
+              directoryName: createUserDir,
+            },
+          ],
+        },
+      }));
+      set({ createUserDir: '' });
+    } else if (res.httpStatus === 400) {
+      alert(res.message);
+    }
+  },
+  // userdir 삭제
+  postDeleteDir: async () => {
+    const currentDirId = get().currentDirId;
+    const res = await dirAPI.deleteDir([currentDirId]);
+    const userDirs = get().userDirs;
+    const dirInd = userDirs.mainUserDirectory.findIndex(
+      (e) => e.userDirectoryId === currentDirId,
+    );
+
+    if (res.httpStatus === 204) {
       set({ currentDirId: 'm' });
-      console.log('udir 삭제: ', res);
-    },
-    // 담기
-    postMovePlans: async (dirId) => {
-      const checkedPlans = get().checkedPlans;
-      await dirAPI.movePlan(checkedPlans, dirId);
-      set({ checkedPlans: [] });
-    },
-    // userDir 이름 변경
-    postChangeDirName: async () => {
-      const currentDirId = get().currentDirId;
-      const changeDirName = get().changeDirName;
-      const res = await dirAPI.postDirName(currentDirId, changeDirName);
-      console.log('Dir이름변경: ', res);
-    },
-  }) /*),
-    {
-      // 새로고침 후 상태 유지, sessionStorage 사용
-      name: 'dir-storage',
-      getStorage: () => sessionStorage,
-    },*/,
-);
+      set((state) => ({
+        userDirs: {
+          ...state.userDirs,
+          planCount: [
+            ...state.userDirs.planCount.slice(0, dirInd),
+            ...state.userDirs.planCount.slice(dirInd + 1),
+          ],
+          mainUserDirectory: [
+            ...state.userDirs.mainUserDirectory.slice(0, dirInd),
+            ...state.userDirs.mainUserDirectory.slice(dirInd + 1),
+          ],
+        },
+      }));
+    }
+  },
+  // userDir 이름 변경
+  postChangeDirName: async () => {
+    const currentDirId = get().currentDirId;
+    const changeDirName = get().changeDirName;
+    const userDirs = get().userDirs;
+    const res = await dirAPI.postDirName(currentDirId, changeDirName);
+    const dirInd = userDirs.mainUserDirectory.findIndex(
+      (e) => e.userDirectoryId === currentDirId,
+    );
+
+    if (res.httpStatus === 201) {
+      userDirs.mainUserDirectory[dirInd].directoryName = changeDirName;
+      set({ currentDirId: 'm' });
+      set({ changeDirName: '' });
+    } else if (res.httpStatus === 400) {
+      alert(res.message);
+    }
+  },
+}));
